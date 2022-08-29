9ab327af2e4a9a7e49835c2478f0d8f0b89f768f
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-29 13:34:26 +0700

    add solution Task 050

---
+++ Task_050/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же
+// указание, что такого элемента нет.
+Console.Clear();
+
+Random rand = new Random();
+
+void FillMatrix (int[,] matr) //Функция заполнения массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(-15, 15); //ограничение значеий для заполнения
+        }
+    }
+}
+
+void PrintArray(int[,] arr) //Функция вывода массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+       for (int j = 0; j < arr.GetLength(1); j++)
+       {
+            Console.Write($"{arr[i, j]}\t");
+       } 
+       System.Console.WriteLine();
+    }
+}
+
+
+
+int[,] matrix = new int[4, 4]; //Ограничение размера массива
+
+FillMatrix(matrix); //Запуск функции заполнения
+PrintArray(matrix); //Запуск функции печати
+
+System.Console.WriteLine("Введите номер строки: ");
+int posLine = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите номер колонки: ");
+int posColumn = int.Parse(Console.ReadLine());
+
+
+if(posLine >= matrix.GetLength(0) | posColumn >= matrix.GetLength(1))
+{
+
+   System.Console.WriteLine($"Элемента с позициями [{posLine}, {posColumn}] в данном массиве нет ");
+}
+else
+{
+   System.Console.WriteLine($"Элемент [{posLine}, {posColumn}] равен {matrix[posLine, posColumn]} "); 
+}
+   
+
